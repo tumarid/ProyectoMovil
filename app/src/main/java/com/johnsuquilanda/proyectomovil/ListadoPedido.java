eb34c3ad8895198a931845cda7e9cd0c8b91a9be
tumarid <johnsu00@hotmail.com>
2020-09-09 13:06:36 -0500

    proyecto

---
+++ app/src/main/java/com/johnsuquilanda/proyectomovil/ListadoPedido.java
@@ -0,0 +1,14 @@
+package com.johnsuquilanda.proyectomovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListadoPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_pedido);
+    }
+}
